8926649ce9157a8e12566320630dad72b134d8db
Hayri Mert kurt <hayrimertkurt@outlook.com>
2022-07-26 16:58:00 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public delegate void StartTheThread();
+        public EventHandler
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start a second thread.");
+
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread do some work");
+                Thread.Sleep(1000);
+            }
+            Console.ReadLine();
+        }
+
+        private static void ThreadProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                // Yield the rest of the time slice.
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
---
+++ LED Project/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LED_Project
+{
+    public partial class Form1 : Form
+    {
+      
+        
+        Color currentColor = Color.Black;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private async void StartTask()
+        {
+            labelResult.Text = "waiting for task to complete";
+            //await task.run(() => colorchanger());
+            
+        }
+        
+        private void ColorChanger(/*color color1*/)
+        {
+            if (pictureBox1.InvokeRequired)
+            {
+                pictureBox1.BeginInvoke((MethodInvoker)delegate () {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine("timer #1 :" + i + " seconds");
+                        pictureBox1.BackColor = SwapColor(pictureBox1.BackColor);//use delegate here
+                        Console.WriteLine(pictureBox1.BackColor);
+                        Thread.Sleep(1000);
+
+                    }
+                });
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("timer #1 :" + i + " seconds");
+                    pictureBox1.BackColor = SwapColor(pictureBox1.BackColor);//use delegate here
+                    Console.WriteLine(pictureBox1.BackColor);
+                    Thread.Sleep(1000);
+
+                }
+            }
+
+        }
+
+        private void ColorChanger2(/*color color1*/)
+        {
+            if (pictureBox2.InvokeRequired)
+            {
+                pictureBox2.BeginInvoke((MethodInvoker)delegate () {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine("timer #1 :" + i + " seconds");
+                        pictureBox2.BackColor = SwapColor(pictureBox2.BackColor);//use delegate here
+                        Console.WriteLine(pictureBox1.BackColor);
+                        Thread.Sleep(1000);
+
+                    }
+                });
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("timer #1 :" + i + " seconds");
+                    pictureBox2.BackColor = SwapColor(pictureBox2.BackColor);//use delegate here
+                    Console.WriteLine(pictureBox1.BackColor);
+                    Thread.Sleep(1000);
+
+                }
+            }
+           
+
+        }
+
+
+
+        private Color SwapColor(Color color1)
+        {
+            Color localcolor = color1;
+
+            if (localcolor == Color.Black)
+            {
+                localcolor = Color.Yellow;
+
+            }
+            else if (localcolor == Color.Yellow)
+            {
+                localcolor = Color.Black;
+
+            }
+            return localcolor;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+
+            ThreadStart childRef = new ThreadStart(ColorChanger);
+            //Console.WriteLine("In Main:Creating the Child thread");
+            Thread childThread = new Thread(childRef);
+
+            ThreadStart child2Ref = new ThreadStart(ColorChanger2);
+            Thread child2Thread = new Thread(child2Ref);
+
+            childThread.Start();
+            child2Thread.Start();
+            Console.WriteLine(t.Name);
+            Console.ReadLine();
+            
+            //onColorChange += ColorChanger;
+            //onColorChange += ColorChanger1;
+        }
+
+        private void CallToChildRead()
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "child thread";
+            Thread.Sleep(1000);
+            Console.WriteLine("Child thread starts.");
+            Console.WriteLine(t.Name);
+        }
+        private void CallToChild2Read()
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "child thread";
+            Thread.Sleep(1000);
+            Console.WriteLine("Child thread starts.");
+            Console.WriteLine(t.Name);
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //await Task.Run(() => { BlinkTheLed1(); });
+            //await Task.Run(() => { BlinkTheLed2(); });
+        }
+
+        //private async task blinktheled1()
+        //{
+        //    await task.run(() => 
+        //    {
+        //        for (int i = 0; i < 10; i++)
+        //        {
+        //            console.writeline("timer #1 :" + i + " seconds");
+        //            picturebox1.backcolor = swapcolor(picturebox1.backcolor);
+        //            console.writeline(picturebox1.backcolor);
+        //            thread.sleep(1000);
+        //        }
+
+        //    });
+        //    console.writeline("color change completed");
+        //}
+        //private async task blinktheled2()
+        //{
+        //    await task.run(() =>
+        //    {
+        //        for (int i = 0; i < 10; i++)
+        //        {
+        //            console.writeline("timer #1 :" + i + " seconds");
+        //            picturebox2.backcolor = swapcolor1(picturebox1.backcolor);
+        //            console.writeline(picturebox2.backcolor);
+        //            thread.sleep(1000);
+        //        }
+
+        //    });
+        //    console.writeline("color change completed");
+        //}
+    }
+}
